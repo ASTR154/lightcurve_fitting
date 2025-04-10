15c7110515a1b40b4ca1acf7456f61cf322c6ac9
IshaanSatish <64127786+IshaanSatish@users.noreply.github.com>
2025-04-10 12:18:15 -0700

    FIxed 30 lines mostly  by adding whitespaces around assignments and commas

--- lightcurve_fitting/fitting.py
+++ lightcurve_fitting/fitting.py
@@ -9,12 +9,16 @@ from pkg_resources import resource_filename
 import warnings
 prior_warning='The p_max/p_min keywords are deprecated. Use the priors keyword instead.'
 model_kwargs_warning='The model_kwargs keyword is deprecated. These are now included in the model intialization.'
-def lightcurve_mcmc(lc,model,priors=None,p_min=None,p_max=None,p_lo=None,p_up=None,nwalkers=100,nsteps=1000,nsteps_burnin=1000,model_kwargs=None,show=False,save_plot_as='',save_sampler_as='',use_sigma=False,sigma_type='relative'):
+
+#2 lines around function
+def lightcurve_mcmc(lc, model, priors=None, p_min=None, p_max=None, p_lo=None, p_up=None,
+        nwalkers= 100, nsteps= 1000, nsteps_burnin= 1000, model_kwargs=None, show=False, save_plot_as='',
+        save_sampler_as='', use_sigma=False, sigma_type='relative'): # added spacing after comma + spacing after assignments to real values
     if model_kwargs!=None:
         raise Exception(model_kwargs_warning)
-    if model.output_quantity=='flux':
+    if model.output_quantity == 'flux':  #Spaces for readability around ==
         lc.calcFlux()
-    elif model.output_quantity=='lum':
+    elif model.output_quantity == 'lum':#Spaces for readability around ==
         lc.calcAbsMag()
         lc.calcLum()
     if use_sigma and model.input_names[-1]!='\\sigma':
@@ -22,9 +26,9 @@ def lightcurve_mcmc(lc,model,priors=None,p_min=None,p_max=None,p_lo=None,p_up=No
         model.units.append(u.dimensionless_unscaled)
     ndim=model.nparams
     #DEPRECATED
-    if p_min==None:
+    if p_min == None: #Spaces for readability around ==
         p_min=np.tile(-np.inf,ndim)
-    elif len(p_min)==ndim:
+    elif len(p_min) == ndim: #Spaces for readability around ==
         p_min=np.array(p_min,float)
         warnings.warn(prior_warning)
     else:
@@ -32,24 +36,24 @@ def lightcurve_mcmc(lc,model,priors=None,p_min=None,p_max=None,p_lo=None,p_up=No
     #DEPRECATED
     if p_max==None:
         p_max=np.tile(np.inf,ndim)
-    elif len(p_max)==ndim:
+    elif len(p_max) == ndim: #Spaces for readability around ==
         p_max=np.array(p_max,float)
         warnings.warn(prior_warning)
     else:
         raise Exception(prior_warning)
     if p_lo==None:
         p_lo=p_min
-    elif len(p_lo)==ndim:
+    elif len(p_lo) == ndim: #Spaces for readability around ==
         p_lo=np.array(p_lo,float)
     else:
         raise Exception('p_lo must have length {:d}'.format(ndim))
-    if len(p_up)==ndim:
+    if len(p_up) == ndim: #Spaces for readability around ==
         p_up=np.array(p_up,float)
     else:
         raise Exception('p_up must have length {:d}'.format(ndim))
     if priors==None:
         priors=[UniformPrior(p0,p1) for p0,p1 in zip(p_min,p_max)]
-    elif len(priors)!=ndim:
+    elif len(priors) != ndim: #Spaces for readability around !=
         raise Exception('priors must have length {:d}'.format(ndim))
     for param,prior,p0,p1 in zip(model.input_names,priors,p_lo,p_up):
         if p0<prior.p_min:
@@ -57,15 +61,15 @@ def lightcurve_mcmc(lc,model,priors=None,p_min=None,p_max=None,p_lo=None,p_up=No
         if p1>prior.p_max:
             raise Exception(f'starting guess for {param} (p_up={p1}) is outside prior (p_max={prior.p_max})')
     def log_posterior(p):
-        log_prior=0.
-        for prior,p_i in zip(priors,p):
-            log_prior+=prior(p_i)
+        log_prior = 0. #Spaces for readability around =
+        for prior, p_i in zip(priors, p): #Spaces for readability around ,
+            log_prior += prior(p_i)  #Spaces for readability around +=
         if np.isinf(log_prior):
             return log_prior
-        log_likelihood=model.log_likelihood(lc,p,use_sigma=use_sigma,sigma_type=sigma_type)
-        return log_prior+log_likelihood
-    sampler=emcee.EnsembleSampler(nwalkers,ndim,log_posterior)
-    starting_guesses=np.random.rand(nwalkers,ndim)*(p_up-p_lo)+p_lo
+        log_likelihood=model.log_likelihood(lc, p, use_sigma=use_sigma, sigma_type=sigma_type) #Spaces for readability around ,
+        return log_prior + log_likelihood #Spaces for readability 
+    sampler = emcee.EnsembleSampler(nwalkers,ndim,log_posterior) #Spaces for readability 
+    starting_guesses = np.random.rand(nwalkers,ndim)*(p_up-p_lo)+p_lo #Spaces for readability 
     pos,_,_=sampler.run_mcmc(starting_guesses,nsteps_burnin,progress=True,progress_kwargs={'desc':' Burn-in'})
     if show or save_plot_as:
         fig,ax=plt.subplots(ndim,2,figsize=(12.,2.*ndim))
@@ -76,7 +80,7 @@ def lightcurve_mcmc(lc,model,priors=None,p_min=None,p_max=None,p_lo=None,p_up=No
         ax1[0].set_title('During Burn In')
         ax1[-1].set_xlabel('Step Number')
     sampler.reset()
-    sampler.run_mcmc(pos,nsteps,progress=True,progress_kwargs={'desc':'Sampling'},skip_initial_state_check=True)
+    sampler.run_mcmc(pos, nsteps, progress=True, progress_kwargs={'desc':'Sampling'},   skip_initial_state_check=True)
     if save_sampler_as:
         np.save(save_sampler_as,sampler.flatchain)
         print('saving sampler.flatchain as '+save_sampler_as)
@@ -96,7 +100,12 @@ def lightcurve_mcmc(lc,model,priors=None,p_min=None,p_max=None,p_lo=None,p_up=No
         if show:
             plt.show()
     return sampler
-def lightcurve_corner(lc,model,sampler_flatchain,model_kwargs=None,num_models_to_plot=100,lcaxis_posn=(0.7,0.55,0.2,0.4),filter_spacing=1.,tmin=None,tmax=None,t0_offset=None,save_plot_as='',ycol=None,textsize='medium',param_textsize='large',use_sigma=False,xscale='linear',filters_to_model=None,label_filters=True,lc_plot_kwargs=None,model_plot_kwargs=None):
+    
+#2 lines around functions
+def lightcurve_corner(lc, model, sampler_flatchain, model_kwargs=None, num_models_to_plot=100,
+                      lcaxis_posn=(0.7,0.55,0.2,0.4), filter_spacing=1., tmin=None, tmax=None, t0_offset=None, save_plot_as='',
+                      ycol=None, textsize='medium', param_textsize='large', use_sigma=False, xscale='linear', filters_to_model=None,
+                      label_filters=True, lc_plot_kwargs=None, model_plot_kwargs=None): # #Spaces for readability + made it go over multiple lines
     if model_kwargs!=None:
         raise Exception(model_kwargs_warning)
     if ycol==None:
@@ -109,18 +118,18 @@ def lightcurve_corner(lc,model,sampler_flatchain,model_kwargs=None,num_models_to
     axis_labels_corner=model.axis_labels
     for var in ['t_0','t_\\mathrm{max}']:
         if var in model.input_names:
-            i_t0=model.input_names.index(var)
+            i_t0 = model.input_names.index(var) #Spaces for readability 
             if t0_offset==None:
                 t0_offset=np.floor(sampler_flatchain_corner[:,i_t0].min())
-            if t0_offset!=0.:
+            if t0_offset != 0.: #Spaces for readability 
                 sampler_flatchain_corner[:,i_t0]-=t0_offset
                 t0_offset_formatted='{:f}'.format(t0_offset).rstrip('0').rstrip('.')
                 axis_labels_corner[i_t0]=f'${var}-{t0_offset_formatted}$ (d)'
     fig=corner.corner(sampler_flatchain_corner,labels=axis_labels_corner,label_kwargs={'size':textsize})
     corner_axes=np.array(fig.get_axes()).reshape(sampler_flatchain.shape[-1],sampler_flatchain.shape[-1])
     for i in range(sampler_flatchain.shape[-1]):
-        corner_axes[i,0].tick_params(labelsize=textsize)
-        corner_axes[-1,i].tick_params(labelsize=textsize)
+        corner_axes[i,0].tick_params(labelsize = textsize)
+        corner_axes[-1,i].tick_params(labelsize = textsize)
     for ax in np.diag(corner_axes):
         ax.spines['top'].set_visible(False)
         ax.spines['left'].set_visible(False)
@@ -164,11 +173,11 @@ def lightcurve_corner(lc,model,sampler_flatchain,model_kwargs=None,num_models_to
         y_fit1=[None]*len(ufilts)
     if t0_offset==None:
         t0_offset=np.floor(tmin)
-    if ycol=='lum':
-        dycol='dlum'
+    if ycol == 'lum': #Spaces for readability 
+        dycol = 'dlum' #Spaces for readability 
         yscale=10.**np.round(np.log10(y_fit.max()))
         ylabel='Luminosity $L_\\nu$ (10$^{{{:.0f}}}$ erg s$^{{-1}}$ Hz$^{{-1}}$)+Offset'.format(np.log10(yscale)+7)#W-->erg/s
-    elif ycol=='absmag':
+    elif ycol == 'absmag': #Spaces for readability 
         dycol='dmag'
         yscale=1.
         ylabel='Absolute Magnitude+Offset'
@@ -202,10 +211,10 @@ def lightcurve_corner(lc,model,sampler_flatchain,model_kwargs=None,num_models_to
         lc=lc.where(MJD_min=t0_offset)
     else:
         lc=lc.copy()
-    lc['MJD']-=t0_offset
-    lc[ycol]/=yscale
-    lc[dycol]/=yscale
-    lc.plot(xcol='MJD',ycol=ycol,offset_factor=filter_spacing,appmag_axis=False,tight_layout=False,**lc_plot_kwargs)
+    lc['MJD'] -= t0_offset #Spaces for readability 
+    lc[ycol] /= yscale #Spaces for readability 
+    lc[dycol] /= yscale #Spaces for readability 
+    lc.plot(xcol='MJD', ycol=ycol, offset_factor=filter_spacing, appmag_axis=False, tight_layout=False, **lc_plot_kwargs) #Spaces for readability 
     plt.autoscale(False)
     _,labels,_=filter_legend(np.array(ufilts),filter_spacing)
     for yfit,yfit1,filt,txt in zip(y_fit,y_fit1,ufilts,labels):
@@ -220,9 +229,9 @@ def lightcurve_corner(lc,model,sampler_flatchain,model_kwargs=None,num_models_to
     ax.tick_params(labelsize=textsize)
     #format credible intervals
     sigfigs=1;percentiles=(15.87,50.,84.14);axis=0
-    quantiles=np.percentile(sampler_flatchain,percentiles,axis=axis).T
+    quantiles = np.percentile(sampler_flatchain, percentiles, axis=axis).T #Spaces for readability 
     uncertainties=np.diff(quantiles)
-    smaller_unc=np.amin(uncertainties,axis=-1)
+    smaller_unc=np.amin(uncertainties, axis=-1) #Spaces for readability 
     log_unc=np.log10(smaller_unc)
     roundto=sigfigs-np.floor(log_unc).astype(int)-1
     #catch numbers that will have more sigfigs after rounding
@@ -247,7 +256,7 @@ def lightcurve_corner(lc,model,sampler_flatchain,model_kwargs=None,num_models_to
         paramtexts=texstrings
     else:
         paramtexts=[]
-        for var,value,unit in zip(model.input_names,texstrings,model.units):
+        for var,value,unit in zip(model.input_names, texstrings, model.units): #Spaces for readability 
             if isinstance(unit,u.quantity.Quantity):
                 value='({}) \\times 10^{{{:.1f}}}'.format(value,np.log10(unit.value)).replace('.0}','}')
                 unit=unit.unit
